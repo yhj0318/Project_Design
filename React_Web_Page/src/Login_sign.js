7f31a86224a3ebb9ebd384489f013231e66fbbc2
yhj0318 <gudwns0318@naver.com>
2023-12-30 22:45:15 +0900

    12-30 update

--- React_Web_Page/src/Login_sign.js
+++ React_Web_Page/src/Login_sign.js
@@ -3,12 +3,35 @@
  * 진행 사항:
  * 로그인/가입 페이지를 만들었음
  * 아직 아이디랑 비밀번호를 DB에 연결하지 않아 실제 로그인은 미완성
+ * 
+ * 12-30
+ * 진행 사항:
+ * 로그인을 하기 위해 ajax로 통신한다. axios 라이브러리를 사용하였다.
+ * 서버로부터 post요청을 하는 비동기 통신 방법을 채택했다.
+ * 마찬가지로 서버로부터 수신을 받는 코드에 문제가 있는 것 같다. 수정 예정
  */
-import React from 'react';
+import React, { useState } from 'react';
 import './Login_sign.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Login_sign = () => {
+
+    const [id, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+        const response = await axios.post('http://localhost:8080/login_sign', {
+            id,
+            password
+        });
+        alert(response.data);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+  
     return (
         <>
         <div class="login-sign-content">
@@ -21,10 +44,10 @@ const Login_sign = () => {
                 <div class="login-top">로그인</div>
                 <form class="login-box">
                     <label for="id">아이디</label>
-                    <input type='text' id="id" class="input-id" placeholder='아이디를 입력하세요'></input>
+                    <input type='text' id="id" class="input-id" placeholder='아이디를 입력하세요' value={id} onChange={(e) => setUsername(e.target.value)}></input>
                     <label for="password">비밀번호</label>
-                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 입력하세요'></input>
-                    <button class="login-btn">로그인</button>
+                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 입력하세요' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    <button class="login-btn" onClick={handleLogin}>로그인</button>
                 </form>
                 <div class="sign-box">
                     상담을 위해 회원가입을 진행하세요!
--- React_Web_Page/src/Sign.js
+++ React_Web_Page/src/Sign.js
@@ -4,11 +4,35 @@
  * 회원가입 페이지이다.
  * 아직까진 DB에 연결하지 않아서 제대로 된 기능을 하지 않는 페이지이다.
  * DB연결하여 간단하게 회원가입을 진행하도록 구현 예정이다.
+ * 
+ * 12-30
+ * 진행 사항:
+ * 회원가입 기능을 만들었다.
+ * 로그인과 마찬가지로 axios 라이브러리를 사용하였다.
+ * 비밀번호 확인은 나중에 기능을 추가할 예정이다.
+ * 마찬가지로 서버로부터 수신을 받는 코드에 문제가 있는 것 같다. 수정 예정
  */
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 const Sign = () => {
+
+    const [id, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister = async () => {
+        try {
+        const response = await axios.post('http://localhost:8080/sign', {
+            id,
+            password
+        });
+        console.log(response.data);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
     return (
         <>
         <div class="login-sign-content">
@@ -21,12 +45,12 @@ const Sign = () => {
                 <div class="login-top">회원가입</div>
                 <form class="login-box">
                     <label for="id">아이디</label>
-                    <input type='text' id="id" class="input-id" placeholder='아이디를 입력하세요'></input>
+                    <input type='text' id="id" class="input-id" placeholder='아이디를 입력하세요' value={id} onChange={(e) => setUsername(e.target.value)}></input>
                     <label for="password">비밀번호</label>
-                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 입력하세요'></input>
-                    <label for="password">비밀번호 확인</label>
-                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 다시 입력하세요'></input>
-                    <button class="login-btn">회원가입</button>
+                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 입력하세요' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                    {/* <label for="password">비밀번호 확인</label>
+                    <input type='password' id="password" class="input-pw" placeholder='비밀번호를 다시 입력하세요'></input> */}
+                    <button class="login-btn" onClick={handleRegister}>회원가입</button>
                 </form>
             </div>
         </div>
