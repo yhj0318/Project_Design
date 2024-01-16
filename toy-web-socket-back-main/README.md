c61a32ccc5ed34170e0ffd9e31e3e0cb9e8ff1d4
BeomJun Kang <104546834+kcv8852@users.noreply.github.com>
2024-01-16 16:38:45 +0900

    Delete toy-web-socket-back-main directory

--- toy-web-socket-back-main/README.md
+++
@@ -1,9 +0,0 @@
-# toy-web-socket-back
-
-## node.js
-
-### no DB, this server don't save any data.
-
-### reference
-
-웹소켓 기본 개념: https://woowacourse.github.io/javable/post/2020-09-20-websocket/
--- toy-web-socket-back-main/src/lib/printLog.js
+++
@@ -1,9 +0,0 @@
-const colors = require("colors/safe");
-
-function printLog(data) {
-  console.log(
-    `${colors.yellow(data.type)} is fired with data: ${JSON.stringify(data)}`
-  );
-}
-
-module.exports = printLog;
