f49d177137e0b5ae40d9c6d393036c3cf06a6ca5
yhj0318 <gudwns0318@naver.com>
2024-03-10 23:51:11 +0900

    3-10 update

--- React_Web_Page/src/Consulting.js
+++ React_Web_Page/src/Consulting.js
@@ -1,15 +1,28 @@
+/**
+ * 3-10
+ * 진행 사항:
+ * 예약페이지를 만들 예정이다.
+ * 만들기 이전에 변호사와 일반사용자를 구분지어놓기 위해 회원가입 부분을 먼저 수정하려고 한다.
+ */
 import React from 'react';
 
 const Consulting = () => {
-    const openNewWindow = () => {
-        window.open('http://localhost:3000/', '_blank', 'width=600,height=700');
-      };
+
     return (
-        <div>
-      
-      <button onClick={openNewWindow}>채팅 상담 시작하기</button>
-    </div>
-    
+      <div class="reserve-consulting-main">
+        <div class="reserve-consulting-top">
+          <div class="reserve-consulting-">
+
+          </div>
+        </div>
+        <div class="reserve-consulting-mid">
+
+        </div>
+        <div class="reserve-consulting-bottom">
+
+        </div>
+      </div>
+
     );
   };
   
